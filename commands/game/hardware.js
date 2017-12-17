9bfdaf25e0fff3ecf97e2fa8727c8cd13093d9ca
funnbot <dillonbayless1@gmail.com>
2017-12-17 11:20:46 -0800

    lit

--- commands/game/hardware.js
+++ commands/game/hardware.js
@@ -1,19 +1,19 @@
-const Clicker = require("../../Clicker.js")
-const {
-  dcm,
-  PRODUCERS_keys,
-  PRODUCERS
-} = require("../../config.js")
+const Clicker = require("../../Clicker.js");
+const {dcm, RODUCERS_keys, PRODUCERS} = require("../../config.js");
 
 module.exports = new Clicker.Command({
-  help: "Buy mining hardware to increase production per second.",
-  example: "${p}hardware",
-  argExample: "none",
-  userPerms: 0,
-  botPerms: ["EMBED_LINKS"],
-  alias: ["buy", "producers"],
+    help: "Buy mining hardware to increase production per second.",
+    example: "${p}hardware",
+    argExample: "none",
+    userPerms: 0,
+    botPerms: ["EMBED_LINKS"],
+    alias: ["buy", "producers"],
 
-  run: async (message, bot, send) => {
-    let embed = new bot.Embed()
-  }
-})
+    run: async (message, bot, send) => {
+        const user = message.author;
+        const embed = new bot.Embed();
+        embed.setAuthor(user.tag, user.displayAvatarURL())
+            .setTitle("Buy Hardware")
+            .setDescription()
+    }
+});
